246717de270b91be1d62d04b4b5f17b2b4191a09
JohnNadja <cv3802@innovaccion.mx>
2022-04-21 18:07:39 -0500

    6: writing a test expecting to function

--- spiderverse/test/spiderman.test.js
+++ spiderverse/test/spiderman.test.js
@@ -1,6 +1,13 @@
 describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+    test('1) Create a spiderman object', () => {
+        // Aquí escribimos el código que queremos usar tal cuál
+        // Quiero poder instanciar un objeto Spiderman con esta información
+        const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+      //Validamos que funcione este código de la forma esperada
+      expect(resultOfSomething).toBe("Spiderman Sony");
+      expect(resultOfSomething).toBe(31);
+      expect(resultOfSomething).toBe("Andrew Garfield");
+      expect(resultOfSomething).toBe(2);
+      expect(resultOfSomething).toBe("Sony");
     });
   })
